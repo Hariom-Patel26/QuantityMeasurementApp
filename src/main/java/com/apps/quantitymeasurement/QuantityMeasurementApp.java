3a36950ce5a83a294cfb2e96990c6c121838a1fc
Hariompatel26 <hariompatel2611@gmail.com>
2026-02-21 14:41:26 +0530

    [Hariom] : Added a program files of UC2 - InchEquality.

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -3,13 +3,25 @@ package com.apps.quantitymeasurement;
 public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
+    	
+    	// UC1 - FeetEquality
 
         Feet feet1 = new Feet(1.0);
         Feet feet2 = new Feet(1.0);
 
-        boolean result = feet1.equals(feet2);
+        boolean feetResult = feet1.equals(feet2);
 
         System.out.println("Input: 1.0 ft and 1.0 ft");
-        System.out.println("Output: Equal (" + result + ")");
+        System.out.println("Output: Equal (" + feetResult + ")");
+        
+        // UC2 - InchEquality
+
+        Inches inch1 = new Inches(1.0);
+        Inches inch2 = new Inches(1.0);
+
+        boolean inchResult = inch1.equals(inch2);
+
+        System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Output: Equal (" + inchResult + ")");
     }
 }
