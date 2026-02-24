47e7389eed3ee55911f8bde67cee357be1b3b5df
Hariompatel26 <hariompatel2611@gmail.com>
2026-02-24 10:39:03 +0530

    [Hariom] : Added a program files of feature/UC10-Generic-Quantity-Class.

--- src/main/java/com/apps/quantitymeasurement/WeightUnit.java
+++ src/main/java/com/apps/quantitymeasurement/WeightUnit.java
@@ -1,26 +1,39 @@
 package com.apps.quantitymeasurement;
 
-public enum WeightUnit {
-	KILOGRAM(1.0), GRAM(0.001),  POUND(0.453592) ;
+public enum WeightUnit implements IMeasurable {
+	
+	KILOGRAM(1.0),
+	GRAM(0.001),
+	POUND(0.453592);
+
 	private final double conversionFactor;
 
 	WeightUnit(double conversionFactor) {
 		this.conversionFactor = conversionFactor;
 	}
 
+	@Override
 	public double getConversionFactor() {
 		return conversionFactor;
 	}
 
-    public double convertToBaseUnit(double value) {
-        return value * conversionFactor ;
-    } 
-    
-    public double convertFromBaseUnit(double baseValue) {
-        return baseValue /conversionFactor;
-    }
-    public double convert(double value, LengthUnit targetUnit) {
-        double feetValue = this.convertToBaseUnit(value);
-        return feetValue / targetUnit.getConversionFactor();
-    }
+	@Override
+	public double convertToBaseUnit(double value) {
+		return value * conversionFactor;
+	}
+
+	@Override
+	public double convertFromBaseUnit(double baseValue) {
+		return baseValue / conversionFactor;
+	}
+
+	public double convert(double value, LengthUnit targetUnit) {
+		double feetValue = this.convertToBaseUnit(value);
+		return feetValue / targetUnit.getConversionFactor();
+	}
+
+	@Override
+	public String getUnitName() {
+		return this.getUnitName();
+	}
 }
